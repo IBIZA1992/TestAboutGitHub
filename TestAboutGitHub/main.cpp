b1d2c784ad9741197118ae03e46748e645045c2d
JiangNan <jn200319@outlook.com>
2022-07-14 14:12:47 +0800

    Initial Commit

---
+++ TestAboutGitHub/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestAboutGitHub
+//
+//  Created by JiangNan on 2022/7/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
